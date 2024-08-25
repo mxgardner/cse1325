69e2e11dc82f899a8e10e45941f4aceaa84cfda6
Mariah <mariahgardner00@gmail.com>
2024-08-25 11:49:41 -0500

    Correct errors

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Enter your name: ");
         Scanner in = new Scanner(System.in);
         String name;
-        name = in.nextString();
-        System.out.printf("hello, %s", name);
+        name = in.nextLine();
+        System.out.printf("hello, %s\n", name);
     }
 }
