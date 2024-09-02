401a2fdc8148e1e2ede77664001a19d655753269
Mariah <mariahgardner00@gmail.com>
2024-09-02 16:59:34 -0500

    Add methods

---
+++ P02/full_credit/Purse.java
@@ -0,0 +1,28 @@
+enum Denomination { Penny = 0.01, Nickel = 0.05, Dime 0.10, Quarter = 0.25, Dollar = 1.0 
+};
+
+public class Coin{
+    private Denomination denomination;
+    private int year;
+    
+    public Coin(int year, Denomination denomination){
+        this.year = year;
+        this.denomination = denomination;
+
+        // TODO: Add data validation
+    }
+
+    public double getValue{
+        return denomination;
+    }
+
+    public double getYear{
+        
+    }
+}
+public class Purse {
+
+    public static void main(string[] args) {
+
+    }
+}
