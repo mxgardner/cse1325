d14a68b2828d363951033bd5a351f5b6f9ddce81
Mariah <mariahgardner00@gmail.com>
2024-09-02 16:48:18 -0500

    Create fields and constructor for Class Coin

--- P02/full_credit/Coin.java
+++ P02/full_credit/Coin.java
@@ -1,6 +1,11 @@
 enum Denomination { Penny = 0.01, Nickel = 0.05, Dime 0.10, Quarter = 0.25, Dollar = 1.0 
 };
 
+public class Coin{
+    private Denomination denomination;
+    private int year;
+    
+}
 public class Purse {
 
     public static void main(string[] args) {
