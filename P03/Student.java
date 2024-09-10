c8bb4becc57251e15d1ad3399500ecdfceb9c9ae
Mariah <mariahgardner00@gmail.com>
2024-09-09 22:18:04 -0500

    Write Student Class

--- P03/Student.java
+++ P03/Student.java
@@ -4,5 +4,23 @@ public class Student{
     private String email;
     private Account account;
 
-    
+    public Student(){
+        if (!email.endsWith("@uta.edu") && !email.endsWith("@mavs.uta.edu")) {
+            throw new IllegalArgumentException("Non-UTA email address: " + email);
+        }
+
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.account = new Account();
+    }
+
+    public String requestMedia(Media media) {
+        return account.play(media); 
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d, %s, Account #%d)", name, id, email, account.getAccountNumber());
+    }
 }
