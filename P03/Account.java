16a1f898d26956718df7ca17fcecb0555b71be79
Mariah <mariahgardner00@gmail.com>
2024-09-09 21:50:35 -0500

    Write Account class

--- P03/Account.java
+++ P03/Account.java
@@ -1,12 +1,18 @@
 public class Account {
     private int accountNumber;
-    private static int nextAccountNumber;
+    private static int nextAccountNumber = 1;
 
-    public Account (int accountNumber, int nextAccountNumber){
-        this.accountNumber = accountNumber;
-        this.nextAccountNumber = nextAccountNumber;
-
+    public Account() (int accountNumber, int nextAccountNumber){
+        this.accountNumber = nextAccountNumber++;
         // TODO: Add data validation
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String play(Media media) {
+        return "Playing " + media.toString();
     }
 
 }
