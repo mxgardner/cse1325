319fc64fd90c7654b97656a5ab607081a8c90f0d
Mariah <mariahgardner00@gmail.com>
2024-09-16 17:24:20 -0500

    Remove DRY code

--- P04/test/TestAccount.java
+++ P04/test/TestAccount.java
@@ -4,22 +4,19 @@ import customer.Account;
 
 public class TestAccount {
     public static void main(String[] args) {
-        Account account1 = new Account();
-        int expected1 = 1;
-        int actual1 = account1.getAccountNumber();
 
-        if (actual1 != expected1) {
-            System.err.println("FAIL: Expected account number '" + expected1 + "', but got '" + actual1 + "'");
-            System.exit(1);
-        }
+        int testCases = 2;
 
-        Account account2 = new Account();
-        int expected2 = 2;
-        int actual2 = account2.getAccountNumber();
+        for(int i = 1; i <= testCases; i++){
+            int expected = i;
+            Account account = new Account();
 
-        if (actual2 != expected2) {
-            System.err.println("FAIL: Expected account number '" + expected2 + "', but got '" + actual2 + "'");
-            System.exit(1); 
+            int actual = account.getAccountNumber();
+
+            if (actual != expected){
+                System.err.println("FAIL: Expected account number '" + expected + "', but got '" + actual + "'");
+                System.exit(1); 
+            }
         }
 
     }
