8091cf23266403f12d4e8fc4e0a0100b3ee0a7e3
Mariah <mariahgardner00@gmail.com>
2024-09-16 22:16:17 -0500

    Build media library

--- P04/moes/Moes.java
+++ P04/moes/Moes.java
@@ -1,5 +1,24 @@
 package moes;
 
+import java.util.ArrayList;
+import product.Media;
+import customer.Student;
+
 public class Moes{
+    private ArrayList<Media> library = new ArrayList<>(); 
+    private ArrayList<Student> customers = new ArrayList<>();
+
+    public void addMedia(Media media) {
+        library.add(media);
+    }
     
+    public void getMediaList() {
+        StringBuilder mediaList = new StringBuilder();  // StringBuilder for better performance
+        for (int i = 0; i < library.size(); i++) {
+            mediaList.append(i)
+                    .append(": ")
+                    .append(library.get(i).toString())
+                    .append("\n");
+        }
+    }
 }
