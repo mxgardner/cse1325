8d15427a60f718b397cd327091c3f13d08c92f05
Mariah <mariahgardner00@gmail.com>
2024-09-16 21:02:51 -0500

    Write Alacarte

--- P04/customer/Alacarte.java
+++ P04/customer/Alacarte.java
@@ -1,5 +1,31 @@
 package customer;
 
+import customer.Media;
+
 public class Alacarte {
-    
+    private int pointsRemaining;
+
+    public alacarte(int pointsRemaining) {
+        this.pointsRemaining = pointsRemaining;
+    }
+
+    public buyPoints (int points){
+        pointsRemaining += points;
+    }
+
+    public getPointsRemaining{
+        return pointsRemaining;
+    }
+
+    @Override
+    public String play(media Media){
+        int pointsRequired = 100;
+
+        if(pointsRemaining.equals pointsRequired || pointsRemaining > pointsRequired){
+            return "Playing " + media.toString();
+        }
+        else{
+            return "Uh oh, You need more points! This media requires '"+ pointsRequired +". You have '" pointsRemaining" "
+        }
+    }
 }
