eb11381163e5704b39ff3eb50419e89b2e611721
Mariah <mariahgardner00@gmail.com>
2024-09-28 17:08:11 -0500

    Write empty methods

--- P05/mdi/Main.java
+++ P05/mdi/Main.java
@@ -1,7 +1,54 @@
 package mdi;
 
+import moes;
+import customer;
+import product;
+
 public class Main{
-    public static void main (String [args]){
-        
+    private moes = new Moes;
+    private String output;
+    private menu = new Menu;
+    private boolean running;
+
+    private void addStudent(Student student){
+
+    }
+
+    private void listStudents(Student students){
+
+    }
+
+    private void addMedia(){
+
+    }
+
+    private void playMedia(){
+
+    }
+
+    private void listMedia(){
+
+    }
+
+    private void listAvailablePoints(){
+
+    }
+
+    private void buyPoints(){
+
+    }
+
+    public Main {
+        public static void main(String args[]){
+
+        }
+
+        public void mdi(){
+
+        }
+
+        public void endapp(){
+            
+        }
     }
 }
