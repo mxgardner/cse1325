a2f28ba94a3bef0af911f5959c0077fd802e88e2
Mariah <mariahgardner00@gmail.com>
2024-09-29 12:11:00 -0500

    Attempt Main.addStudent from memory

--- P05/mdi/Main.java
+++ P05/mdi/Main.java
@@ -3,14 +3,43 @@ package mdi;
 import moes;
 import customer;
 import product;
+import java.util.Scanner;
 
 public class Main{
     private moes = new Moes;
     private String output;
     private menu = new Menu;
     private boolean running;
+    Scanner scanner = new Scanner(System.in);
 
-    private void addStudent(Student student){
+
+    private void addStudent(){
+        System.out.printf("\nStudent name? ");
+        String studentName = scanner.nextLine();
+
+        System.out.printf("\nStudent ID? ");
+        long studentID = scanner.nextLine();
+
+        System.out.printf("\nStudent email? ");
+        String studentEmail = scanner.nextLine();
+
+        System.out.printf("\n(a)lacarte or (u)nlimited ");
+        String studentAccountType = scanner.nextLine();
+
+        boolean studentUnlimited = false;
+        if(studentAccountType.equals("u")){
+            studentUnlimited = true;
+        }
+        else if(studentAccountType.equals("a")){
+            studentUnlimited = false;
+        }
+        else{
+            System.err.println("Invalid Account Type");
+        }
+
+        Student student = new Student(studentName, studentID, studentEmail, studentAccountType);
+
+        Moes.addStudent(Student student);
 
     }
 
@@ -48,7 +77,7 @@ public class Main{
         }
 
         public void endapp(){
-            
+
         }
     }
 }
