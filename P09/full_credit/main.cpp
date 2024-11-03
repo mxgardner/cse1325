b5e948366c189d9a2084d91a29dc4c9116086a78
Mariah <mariahgardner00@gmail.com>
2024-11-03 12:49:27 -0600

    Complete Main function

--- P09/full_credit/main.cpp
+++ P09/full_credit/main.cpp
@@ -8,5 +8,24 @@ int main(int argc, char* argv[]) {
         std::cerr << "usage: clock <hour> <minutes> <seconds>" << std::endl;
         return -1;
     }
+    
+    try {
+        int hours = std::stoi(argv[1]);
+        int minutes = std::stoi(argv[2]);
+        int seconds = std::stoi(argv[3]);
+        Clock clock(hours, minutes, seconds);
 
+        std::string input;
+        while (true) {
+            clock.print();
+            std::getline(std::cin, input);
+            if (input == "q") break;
+            clock.tic();
+        }
+    } catch (const std::out_of_range& e) {
+        std::cerr << e.what() << std::endl;
+        return -2;
+    }
+
+    return 0;
 }
