1c18de6f729f1e45bb3bdfaad38772c357032d4d
Mariah <mariahgardner00@gmail.com>
2024-11-03 12:37:07 -0600

    Set-up main

---
+++ P09/full_credit/main.cpp
@@ -0,0 +1,12 @@
+#include "clock.h"
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "usage: clock <hour> <minutes> <seconds>" << std::endl;
+        return -1;
+    }
+    return 0;
+}
