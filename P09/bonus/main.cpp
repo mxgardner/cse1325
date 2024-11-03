1bbab97f9d1bef45322915f9b8d3f643ed6e7f0c
Mariah <mariahgardner00@gmail.com>
2024-11-03 12:56:59 -0600

    Make adjustments to main for timer

---
+++ P09/bonus/main.cpp
@@ -0,0 +1,28 @@
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "usage: clock <hour> <minutes> <seconds>" << std::endl;
+        return -1;
+    }
+
+    try {
+        int hours = std::stoi(argv[1]);
+        int minutes = std::stoi(argv[2]);
+        int seconds = std::stoi(argv[3]);
+        Timer timer(hours, minutes, seconds);
+
+        std::string input;
+        while (true) {
+            timer.print();
+            std::getline(std::cin, input);
+            if (input == "q") break;
+            timer.tic();
+        }
+    } catch (const std::out_of_range& e) {
+        std::cerr << e.what() << std::endl;
+        return -2;
+    } catch (const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
