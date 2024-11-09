6c4f81be60c95c0c2875d1120794b9c67a1d0bad
Mariah <mariahgardner00@gmail.com>
2024-11-09 16:10:49 -0600

    Add input stream to Purse.h

---
+++ P10/bonus/Purse.h
@@ -0,0 +1,26 @@
+#ifndef PURSE_H
+#define PURSE_H
+
+#include <iostream>
+#include <compare>
+
+class Purse {
+private:
+    int _pounds, _shillings, _pence;
+    void rationalize();
+
+public:
+    Purse(int pounds = 0, int shillings = 0, int pence = 0) : _pounds(pounds), _shillings(shillings), _pence(pence) { rationalize(); }
+
+    friend std::ostream& operator<<(std::ostream& os, const Purse& purse);
+    friend std::istream& operator>>(std::istream& is, Purse& purse);
+    auto operator<=>(const Purse&) const = default;
+    Purse& operator++();
+    Purse operator++(int);
+    Purse operator+(const Purse&) const;
+    Purse operator-(const Purse&) const;
+    Purse& operator+=(const Purse&);
+    Purse& operator-=(const Purse&);
+};
+
+#endif // PURSE_H
